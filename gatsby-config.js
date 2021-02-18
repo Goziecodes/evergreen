5baf65f6cc0cc0221a96f3b7fb708a633f8ca7d3
goziecodes <ezechukwuchigozie@gmail.com>
2021-02-18 09:51:04 +0100

    updated files

---
+++ gatsby-config.js
@@ -0,0 +1,37 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
+export default {
+  siteMetadata: {
+    title: `kevmiester clothings`,
+    siteUrl: 'https://gatsby.pizza',
+    description:
+      'The best Clothing Store for quality hand made designer clothes',
+  },
+
+  plugins: [
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-react-helmet',
+
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /\.inline\.svg$/,
+        },
+      },
+    },
+    {
+      //   name of plugin im adding
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'ocqq3ytl',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
+};
